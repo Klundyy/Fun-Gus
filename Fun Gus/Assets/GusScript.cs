4d2a061e33168372b77a848774e2fcd51af83b11
JackWilhelm <jgwilhel@ucsc.edu>
2024-10-28 19:47:21 -0700

    Rotation (Locked when stem on ground)

--- Fun Gus/Assets/GusScript.cs
+++ Fun Gus/Assets/GusScript.cs
@@ -5,17 +5,46 @@ using UnityEngine;
 public class GusScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
+    public float rotationSpeed = 0.75f;
     // Start is called before the first frame update
+    public string groundTag = "Ground";
+
+    private bool StemLocked = true;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space)) {
-            myRigidbody.velocity = Vector2.up * 10;
+            if (StemLocked) {
+                myRigidbody.velocity = Vector2.up * 10;
+            }
         }
+        
+        if (Input.GetKey(KeyCode.A)) {
+            if (!StemLocked) {
+                myRigidbody.rotation += rotationSpeed;
+            }
+                
+        } else if (Input.GetKey(KeyCode.D)) {
+            if (!StemLocked) {
+                myRigidbody.rotation -= rotationSpeed;
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag(groundTag)) {
+            StemLocked = true;
+        } 
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        StemLocked = false;
     }
 }
